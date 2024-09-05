042c1ca0a1727308aa60f0c9bebe34d26dfd302d
dieginin <diegobalestra9@gmail.com>
2024-09-05 00:10:26 -0400

    feat: create club.py

---
+++ models/club.py
@@ -0,0 +1,13 @@
+class Club:
+    def __init__(self, tag: str, name: str) -> None:
+        self.tag = tag
+        self.name = name
+
+    def to_dict(self) -> dict[str, str]:
+        return {"tag": self.tag, "name": self.name}
+
+    @staticmethod
+    def from_dict(obj: dict) -> "Club":
+        tag = obj.get("tag") or ""
+        name = obj.get("name") or ""
+        return Club(tag, name)
