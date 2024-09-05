8db3c10322a70c8e8cc1b732ce18a86104ec9bb3
dieginin <diegobalestra9@gmail.com>
2024-09-05 01:40:47 -0400

    update club.py and member.py -> get all info from api and added helpers

--- models/club.py
+++ models/club.py
@@ -1,13 +1,51 @@
+from helpers import from_int, from_list, from_str, to_class
+
+from .member import Member
+
+
 class Club:
-    def __init__(self, tag: str, name: str) -> None:
+    def __init__(
+        self,
+        tag: str,
+        name: str,
+        description: str,
+        type: str,
+        badge_id: int,
+        required_trophies: int,
+        trophies: int,
+        members: list[Member],
+    ) -> None:
         self.tag = tag
         self.name = name
-
-    def to_dict(self) -> dict:
-        return {"tag": self.tag, "name": self.name}
+        self.description = description
+        self.type = type
+        self.badge_id = badge_id
+        self.required_trophies = required_trophies
+        self.trophies = trophies
+        self.members = members
 
     @staticmethod
     def from_dict(obj: dict) -> "Club":
-        tag = obj.get("tag") or ""
-        name = obj.get("name") or ""
-        return Club(tag, name)
+        tag = from_str(obj.get("tag"))
+        name = from_str(obj.get("name"))
+        description = from_str(obj.get("description"))
+        type = from_str(obj.get("type"))
+        badge_id = from_int(obj.get("badgeId"))
+        required_trophies = from_int(obj.get("requiredTrophies"))
+        trophies = from_int(obj.get("trophies"))
+        members = from_list(Member.from_dict, obj.get("members"))
+        return Club(
+            tag, name, description, type, badge_id, required_trophies, trophies, members
+        )
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["tag"] = from_str(self.tag)
+        result["name"] = from_str(self.name)
+        result["description"] = from_str(self.description)
+        result["type"] = from_str(self.type)
+        result["badgeId"] = from_int(self.badge_id)
+        result["requiredTrophies"] = from_int(self.required_trophies)
+        result["trophies"] = from_int(self.trophies)
+        result["members"] = from_list(lambda x: to_class(Member, x), self.members)
+        return result
--- models/member.py
+++ models/member.py
@@ -1,15 +1,63 @@
-class Member:
-    def __init__(self, tag: str, name: str, trophies: int) -> None:
-        self.tag = tag
-        self.name = name
-        self.trophies = trophies
+from enum import Enum
+
+from helpers import from_int, from_str, to_class, to_enum
+
+
+class Icon:
+    def __init__(self, id: int) -> None:
+        self.id = id
+
+    @staticmethod
+    def from_dict(obj: dict) -> "Icon":
+        id = from_int(obj.get("id"))
+        return Icon(id)
 
     def to_dict(self) -> dict:
-        return {"tag": self.tag, "name": self.name, "trophies": self.trophies}
+        result: dict = {}
+        result["id"] = from_int(self.id)
+        return result
+
+
+class Role(Enum):
+    MEMBER = "member"
+    PRESIDENT = "president"
+    SENIOR = "senior"
+    VICE_PRESIDENT = "vicePresident"
+
+
+class Member:
+    def __init__(
+        self,
+        tag: str,
+        name: str,
+        name_color: str,
+        role: Role,
+        trophies: int,
+        icon: Icon,
+    ) -> None:
+        self.tag = tag
+        self.name = name
+        self.name_color = name_color
+        self.role = role
+        self.trophies = trophies
+        self.icon = icon
 
     @staticmethod
     def from_dict(obj: dict) -> "Member":
-        tag = obj.get("tag") or ""
-        name = obj.get("name") or ""
-        trophies = obj.get("trophies") or 0
-        return Member(tag, name, trophies)
+        tag = from_str(obj.get("tag"))
+        name = from_str(obj.get("name"))
+        name_color = from_str(obj.get("nameColor"))
+        role = Role(obj.get("role"))
+        trophies = from_int(obj.get("trophies"))
+        icon = Icon.from_dict(obj.get("icon", {}))
+        return Member(tag, name, name_color, role, trophies, icon)
+
+    def to_dict(self) -> dict:
+        result: dict = {}
+        result["tag"] = from_str(self.tag)
+        result["name"] = from_str(self.name)
+        result["nameColor"] = from_str(self.name_color)
+        result["role"] = to_enum(Role, self.role)
+        result["trophies"] = from_int(self.trophies)
+        result["icon"] = to_class(Icon, self.icon)
+        return result
