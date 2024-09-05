49906eb9c34280c95f6d4518ad21d906836bd303
dieginin <diegobalestra9@gmail.com>
2024-09-05 02:44:45 -0400

    update brawl_api.py -> get_player_battlelog added

--- services/brawl_api.py
+++ services/brawl_api.py
@@ -1,7 +1,7 @@
 import requests
 
 from config import BS_KEY
-from models import Club, Member, Player
+from models import BattleLog, Club, Member, Player
 
 __BASE_URL = "https://bsproxy.royaleapi.dev/v1/"
 CLUB_BASE_URL = __BASE_URL + "clubs/%23"
@@ -18,28 +18,33 @@ def make_request(url: str) -> dict | None:
 
 class BrawlApi:
     def get_club(self, tag: str) -> Club:
-        tag = parse_tag(tag)
-        url = CLUB_BASE_URL + tag
+        url = CLUB_BASE_URL + parse_tag(tag)
         response = make_request(url)
 
         if response:
             return Club.from_dict(response)
-        raise Exception(f"Tag #{tag} no encontrada")
+        raise Exception("Tag no encontrada")
 
     def get_club_members(self, tag: str) -> list[Member]:
-        tag = parse_tag(tag)
-        url = CLUB_BASE_URL + tag + "/members"
+        url = CLUB_BASE_URL + parse_tag(tag) + "/members"
         response = make_request(url)
 
         if response:
             return [Member.from_dict(m) for m in response["items"]]
-        raise Exception(f"Tag #{tag} no encontrada")
+        raise Exception("Tag no encontrada")
 
     def get_player(self, tag: str) -> Player:
-        tag = parse_tag(tag)
-        url = PLYR_BASE_URL + tag
+        url = PLYR_BASE_URL + parse_tag(tag)
         response = make_request(url)
 
         if response:
             return Player.from_dict(response)
         raise Exception(f"Tag #{tag} no encontrada")
+
+    def get_player_battlelog(self, tag: str) -> list[BattleLog]:
+        url = PLYR_BASE_URL + parse_tag(tag) + "/battlelog"
+        response = make_request(url)
+
+        if response:
+            return [BattleLog.from_dict(m) for m in response["items"]]
+        raise Exception("Tag no encontrada")
