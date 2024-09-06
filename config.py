d5edf63363c860a7f6de3f2593ff423f8b80f14c
dieginin <diegobalestra9@gmail.com>
2024-09-06 02:01:52 -0400

    update config.py -> DB_CONFIG added

--- config.py
+++ config.py
@@ -6,3 +6,13 @@ load_dotenv()
 
 BS_KEY = os.getenv("BS_KEY")
 DS_KEY = os.getenv("DS_KEY")
+DB_CONFIG = {
+    "apiKey": os.getenv("DB_KEY"),
+    "authDomain": f"{os.getenv("PROJECTID")}.firebaseapp.com",
+    "databaseURL": os.getenv("DB_URL"),
+    "projectId": os.getenv("PROJECTID"),
+    "storageBucket": f"{os.getenv("PROJECTID")}.appspot.com",
+    "messagingSenderId": os.getenv("MSG_SNDR_ID"),
+    "appId": os.getenv("APPID"),
+    "measurementId": os.getenv("MSRMTID"),
+}
